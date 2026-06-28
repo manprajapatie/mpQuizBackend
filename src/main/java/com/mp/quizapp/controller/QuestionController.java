a0abf7a3e36f26a6508465baecfe6f1337c66c6d
manprajapatie <manprajapatie@gmail.com>
2026-06-28 18:11:06 +0530

    chore : setup controller and service with HTTP Status for better control

--- src/main/java/com/mp/quizapp/controller/QuestionController.java
+++ src/main/java/com/mp/quizapp/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.mp.quizapp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +27,7 @@ public class QuestionController {
     // question is our main route and allQuestions we are defining below and getting
     // it from service
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestion() {
+    public ResponseEntity<List<Question>> getAllQuestion() {
         // Returning getAllQuestions Method from questionService (here we are using same
         // method name as it's parent)
         return questionService.getAllQuestions();
@@ -36,26 +37,26 @@ public class QuestionController {
     // with dynamic route {category}
     @GetMapping("category/{category}")
     // because we are taking dynamic value so we have to take it by @PathVariable
-    public List<Question> getQuestionsByCategory(@PathVariable String category) {
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
         return questionService.getQuestionsByCategory(category);
     }
 
     // It will accept question from the client side and @RequestBody send it to body
     // We we want to send data we use @PostMapping
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question) {
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
         return questionService.addQuestion(question);
     }
 
     // We we want to delete Question, we use @PostMapping
     @DeleteMapping("delete/{id}")
-    public String deleteQuestion(@PathVariable Integer id) {
+    public ResponseEntity<String> deleteQuestion(@PathVariable Integer id) {
         return questionService.deleteQuestion(id);
     }
 
     // We we want to update, we use @PutMapping
     @PutMapping("update/{id}")
-    public String updateQuestion(@PathVariable Integer id, @RequestBody Question updatedQuestion) {
+    public ResponseEntity<String> updateQuestion(@PathVariable Integer id, @RequestBody Question updatedQuestion) {
         return questionService.updateQuestion(id, updatedQuestion);
     }
 
--- src/main/java/com/mp/quizapp/service/QuestionService.java
+++ src/main/java/com/mp/quizapp/service/QuestionService.java
@@ -1,9 +1,12 @@
 package com.mp.quizapp.service;
 
+import java.util.ArrayList;
 import java.util.List;
 // import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.mp.quizapp.dao.QuestionDao;
@@ -14,48 +17,65 @@ public class QuestionService {
     @Autowired
     QuestionDao questionDao;
 
-    public List<Question> getAllQuestions() {
-        return questionDao.findAll();
+    // we wrape it with ResponseEntity for check status code if it is okay or not
+    // and also handle the error
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+        }
+
     }
 
-    public List<Question> getQuestionsByCategory(String category) {
-        return questionDao.findByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+        }
     }
 
-    public String addQuestion(Question question) {
+    public ResponseEntity<String> addQuestion(Question question) {
         // it will save the question from the client
         questionDao.save(question);
-        return "success";
+        return new ResponseEntity<>("success", HttpStatus.CREATED);
     }
 
     // It will delete the object
-    public String deleteQuestion(Integer id) {
-        if (questionDao.existsById(id)) {
-            questionDao.deleteById(id);
-            return "Question deleted successfully.";
+    public ResponseEntity<String> deleteQuestion(Integer id) {
+        if (!questionDao.existsById(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Question not found.");
         }
-        return "Question not found.";
+
+        questionDao.deleteById(id);
+        return ResponseEntity.ok("Question deleted successfully.");
     }
 
     // It will update the object
-    public String updateQuestion(Integer id, Question updatedQuestion) {
+    public ResponseEntity<String> updateQuestion(Integer id, Question updatedQuestion) {
 
         Question question = questionDao.findById(id).orElse(null);
-        if (question == null) {
-            return "Question not found";
+
+        try {
+            question.setQuestionTitle(updatedQuestion.getQuestionTitle());
+            question.setOption1(updatedQuestion.getOption1());
+            question.setOption2(updatedQuestion.getOption2());
+            question.setOption3(updatedQuestion.getOption3());
+            question.setOption4(updatedQuestion.getOption4());
+            question.setRightAnswer(updatedQuestion.getRightAnswer());
+            question.setDifficultyLevel(updatedQuestion.getDifficultyLevel());
+            question.setCategory(updatedQuestion.getCategory());
+
+            questionDao.save(question);
+
+            return new ResponseEntity<>("Question updated successfully", HttpStatus.ACCEPTED);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>("Failed to Update", HttpStatus.BAD_REQUEST);
         }
-        question.setQuestionTitle(updatedQuestion.getQuestionTitle());
-        question.setOption1(updatedQuestion.getOption1());
-        question.setOption2(updatedQuestion.getOption2());
-        question.setOption3(updatedQuestion.getOption3());
-        question.setOption4(updatedQuestion.getOption4());
-        question.setRightAnswer(updatedQuestion.getRightAnswer());
-        question.setDifficultyLevel(updatedQuestion.getDifficultyLevel());
-        question.setCategory(updatedQuestion.getCategory());
 
-        questionDao.save(question);
-
-        return "Question updated successfully";
     }
 
 }
