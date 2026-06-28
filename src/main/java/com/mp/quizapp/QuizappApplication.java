df3a98dfe539a981ae984327d1a0d51af1383bbc
manprajapatie <manprajapatie@gmail.com>
2026-06-28 06:52:21 +0530

    chore : connect database

--- src/main/java/com/mp/quizapp/QuizappApplication.java
+++ src/main/java/com/mp/quizapp/QuizappApplication.java
@@ -3,10 +3,15 @@ package com.mp.quizapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class QuizappApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach((entry) -> System.setProperty(
+				entry.getKey(), entry.getValue()));
 		SpringApplication.run(QuizappApplication.class, args);
 	}
 
