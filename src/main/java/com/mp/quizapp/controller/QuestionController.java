97531f5eeaa7a89920f414f6a8847d02c0fc04ec
manprajapatie <manprajapatie@gmail.com>
2026-06-28 15:47:17 +0530

    chore : add route of category with custom method

--- src/main/java/com/mp/quizapp/controller/QuestionController.java
+++ src/main/java/com/mp/quizapp/controller/QuestionController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +18,18 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
 
+    //With this we will get all quation with route of /question/allQuestions >> question is our main route and allQuestions we are defining below and getting it from service
     @GetMapping("allQuestions")
     public List<Question> getAllQuestion() {
         //Returning getAllQuestions Method from questionService (here we are using same method name as it's parent)
         return questionService.getAllQuestions();
+    }
+
+    //We want result based on catagory that's why we are giving it another route with dynamic route {category}
+    @GetMapping("category/{category}")
+    //because we are taking dynamic value so we have to take it by @PathVariable 
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
     }
 
 }
--- src/main/java/com/mp/quizapp/dao/QuestionDao.java
+++ src/main/java/com/mp/quizapp/dao/QuestionDao.java
@@ -1,5 +1,7 @@
 package com.mp.quizapp.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.mp.quizapp.entities.Question;
 
 @Repository
 public interface QuestionDao extends JpaRepository<Question, Integer> {
+
+    List<Question> findByCategory(String category);
 
 }
